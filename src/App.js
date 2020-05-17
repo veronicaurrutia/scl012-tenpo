dcef0598338d1a364030472869ffc5c753aad936
Corina <corinacvp@gmail.com>
2020-05-16 21:04:25 -0400

    component view registry

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/Welcome.jsx'
+import Welcome from './components/Welcome.jsx';
+import Registry from './components/Registry.jsx';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Welcome />
-    </>
+      <Router >   
+        <Switch>
+          <Route path="/" exact>
+            <Welcome />          
+          </Route>
+          <Route path="/registrarse">
+            <Registry />
+          </Route>
+        </Switch>  
+    </Router>
   );
 }
 
